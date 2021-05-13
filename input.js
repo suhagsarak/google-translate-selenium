28d9f7af64fc6afd7b90f4bdc3f50df820922960
suhag_sarak <suhag.sarak@phenompeople.com>
2021-05-13 18:39:49 +0530

    inout

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 
 exports.literals = [
-
-    
-
+    // //  Example
+    // {
+    //     literalId: "crm-interview-scheduling-please-remove-random-tokens",
+    //     message: "Please remove random tokens"
+    // }
 ];
